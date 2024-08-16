1a14199a75ff88e43e5855d030bcb6f071ebece4
Janina Wibker <me@janina.lol>
2024-08-16 16:12:30 +0200

    wrap rrule package because auf faux esm support

---
+++ src/rrule-wrapper.ts
@@ -0,0 +1,6 @@
+import * as rrule from 'rrule'
+export type { ByWeekday, WeekdayStr, RRuleStrOptions } from 'rrule'
+
+const { RRule, RRuleSet, rrulestr, Frequency, Weekday, ALL_WEEKDAYS, datetime } = rrule
+
+export { RRule, RRuleSet, rrulestr, Frequency, Weekday, ALL_WEEKDAYS, datetime }
